b5262785e96ea9b54cb324d699b3d395d275aee5
Unknown <aebatirel@yahoo.com.tr>
2017-05-23 16:20:28 +0300

    My brain starting to hurt. Writing this fitness function is hard!!

--- individual.m
+++ individual.m
@@ -3,24 +3,46 @@ classdef individual
         routecoodinates
         fitness
         segmentcount
+        pathdistance
     end
     
     methods
-        function obj=individual(start,ending,xdiamension,ydiamension,segment_count)
+        function obj=individual(start,ending,heigthmap,segment_count)
+            [xdiamension,ydiamension]=size(heigthmap);
             obj.routecoodinates=randomroutegenerator(start,ending,xdiamension,ydiamension,segment_count);
             obj.segmentcount=segment_count;
+            
         end
         function fit=fitnesss(heightmap,obj,maxtunelheight,maxbridgeheight,bridgeprice,tunnelprice)
             n=obj.routecoodinates;
+            nx=n(1,:);
             m=size(obj.routecoodinates);
             heightdifferance=heightmap(n(1,m),n(2,m))-heightmap(n(1,1),n(2,1));
             pathlength=0;
-            for i=1:m(2)-1
-                r=segmentheightvalues([n(1,i),n(2,i)],[n(1,i+1),n(2,i+1)],heightmap);
-                pathlength=pathlength+r(3);
+            pathxdiffirance=0;
+            for i=1:size(n(1,:))
+                
+                pathxdiffirance=pathxdiffirance+abs(nx(i)-nx(i+1));
+            end
+            %heightvalues4fitness - siyah kot
+            %desiredvalues4fitness - kýrmýzý kot
+            heightvalues4fitness=zeros(pathxdiffirance);
+            desiredvalues4fitness=zeros(pathxdiffirance);
+            for i=1:size(n)
+                
+                [a,b,c]=segmentheightvalues([n(1,i),n(2,i)],[n(1,i+1),n(2,i+1)],heightmap);
+                pathlength=pathlength+c;
+                for ii=1:size(a)
+                    
+                end
+                
             end
             
+            
+            
+            obj.pathdistance=pathlength;
             fit=0;
+            
         end
         function [route1,route2]=crossoverroute(obj1,obj2)
             if obj1.segmentcount==obj2.segmentcount
